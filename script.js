273865d0ad0d030e76b8a6db873e867ea1561c42
Roger Teletor <RogerTeletor@gmail.com>
2024-10-07 21:40:10 +0200

    doing my first challenge of js

---
+++ script.js
@@ -0,0 +1,23 @@
+/* what i am going to do is create a function that has a loop and tell it that every 15 numbers to put out fizzbuzz 
+and for every 5 number to put out buzz and for every 3 numbers to put out fizz*/
+
+
+
+
+function numberPrinted (number) {
+    for(let i = 1; i < 100;i++) {
+     
+        if (i % 3 ===0){
+           console.log("roger")
+        } 
+        else if (i %5 == 0){
+            console.log("the goat")
+        }
+        else if (i%15 == 0) {
+            console.log("hello!")
+        }
+        console.log(i +" "+ "---"+" "+i)
+    } 
+}
+
+numberPrinted(1)
