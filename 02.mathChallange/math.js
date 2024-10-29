dea0d2fa1e8cd3969c1b5e434dedf62a5e4df64f
Roger Teletor <RogerTeletor@gmail.com>
2024-10-29 09:21:32 +0100

    did a adjustment.

--- 02.mathChallange/math.js
+++ 02.mathChallange/math.js
@@ -1,8 +1,8 @@
 for(let i = 1; i <= 100; i++) {
-  i % 3 === 0 && i % 5 === 0 ? console.log(i,"Roger") : null || 
-  i % 3===0 ? console.log(i,"Cristian") : null ||
-  i % 5===0 ? console.log(i,"Silvia") : null ||
-  console.log(i)
+  i % 3 === 0 && i % 5 === 0 ? console.log(`${i} --- Roger`) : null || 
+  i % 3===0 ? console.log(`${i} --- Cristian`) : null ||
+  i % 5===0 ? console.log(`${i} --- Silvia`) : null ||
+  console.log(`${i} --- ${i}`)
 
 
 
